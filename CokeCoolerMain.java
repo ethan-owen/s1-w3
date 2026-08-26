13218bedbd703cdd786d57329bc33249a07335cf
Ethan Owen <ethanowen848@gmail.com>
2026-08-26 12:35:10 -0700

    fixed

--- CokeCoolerMain.java
+++ CokeCoolerMain.java
@@ -6,13 +6,10 @@ public class CokeCoolerMain {
         n1.refill(249);
         n2.drink(52);
 
-        n1.cokeCount();
-        n2.cokeCount();
+        System.out.println(n1.cokeCount());
+        System.out.println(n2.cokeCount());
 
         n1.drink(125);
         n2.refill(545);
-
-        n1.cokeCount();
-        n2.cokeCount();
     }
 }
