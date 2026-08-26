067ec7375ab60c076409328dc43d6615af5a9703
Ethan Owen <ethanowen848@gmail.com>
2026-08-26 12:05:28 -0700

    asd

--- CandyJar.java
+++ CandyJar.java
@@ -1,3 +1,23 @@
 public class CandyJar {
+    private int numCandies;
+
+    public CandyJar(){
+        numCandies = 10;
+    }
+
+    public CandyJar(int amt){
+        numCandies = amt;
+    }
+
+    public void eatCandy(int amt){
+        numCandies -= amt;
+    }
+
+    public void refillCandy(int amt){
+        numCandies += amt;
+    }
     
+    public void printState(){
+        System.out.println("Candy Amount: " + numCandies);
+    }
 }
--- CandyJarMain.java
+++ CandyJarMain.java
@@ -2,10 +2,16 @@ public class CandyJarMain {
     public static void main(String[] args) {
         CandyJar c1 = new CandyJar();
         CandyJar c2 = new CandyJar(8);
+
         c1.eatCandy(3);
         c2.refillCandy(222);
+
+        c1.printState();
+        c2.printState();
+
         c2.eatCandy(119);
         c1.refillCandy(64);
+        
         c1.printState();
         c2.printState();
     }
