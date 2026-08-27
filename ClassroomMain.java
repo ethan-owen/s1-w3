6d8463f72c918c72ce877382c25b272d4d04a3c2
ethan-owen <ethanowen848@gmail.com>
2026-08-27 22:07:55 +0000

    added main

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,5 +1,9 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        
+        Classroom c1 = new Classroom("Mr. Morris");
+        Classroom c2 = new Classroom("Mr. Davidson", 25, 22);
+
+        c1.setDesks(30);
+        System.out.println(c2.teacher(), c2.desks(), c2.students());
     }
 }
