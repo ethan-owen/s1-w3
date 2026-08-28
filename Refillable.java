803e3bbbd1561bea4b2ff194414ddf529bf6b8a0
Ethan Owen <ethanowen848@gmail.com>
2026-08-28 11:52:55 -0700

    Refillable Done

--- Refillable.java
+++ Refillable.java
@@ -1,3 +1,23 @@
 public class Refillable {
-    
+    private int amount;
+
+    public Refillable(){
+        amount = 10;
+    }
+
+    public Refillable(int amt){
+        amount = amt;
+    }
+
+    public void addTo(int amt){
+        amount += amt;
+    }
+
+    public void useUp(int amt){
+        amount -= amt;
+    }
+
+    public int currentAmt(){
+        return this.amount;
+    }
 }
